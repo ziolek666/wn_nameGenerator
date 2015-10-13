3361d428a847ef02baf34152592a3307a0294ed7
ziolek666 <ziolek666@gmail.com>
2015-10-14 01:23:18 +0200

    kilka nazw

--- zwierzeta.js
+++ zwierzeta.js
@@ -1,17 +1,25 @@
 var maleAnimals = [
-"Łoś", "Żubr", "Bóbr", "Niedźwiedź", "Miś", "Wąż", "Żółw", "Kogut", "Krurczak", "Żuczek", "Gronostaj", "Hipopotam", "Lew"
+"Łoś", "Żubr", "Bóbr", "Niedźwiedź", "Miś", "Wąż", "Żółw", "Kogut", "Krurczak", "Żuczek", "Gronostaj", "Hipopotam", "Lew",
+"Pelikan", "Szop", "Orangutan", "Pawian", "Paw", "Opos", "Mrówkojad", "Struś", "Troll"
 ]
 
 var femaleAnimals = [
-"Anakonda", "Wydra", "Kuna", "Ryjówka", "Łania", "Sarna", "Łasiczka", "Kura", "Koza", "Gęś", "Jaskółka", "Żyrafa", "Lwica"
+"Anakonda", "Wydra", "Kuna", "Ryjówka", "Łania", "Sarna", "Łasiczka", "Kura", "Koza", "Gęś", "Jaskółka", "Żyrafa", "Lwica", "Małpa", "Kotka", "Żółwica", "Niedźwiedzica",
+"Żaba", "Żabka", "Mysz", 
 ]
 
-var maleAdjectives = ["Zwinny", "Szybki", "Srebrny", "Różowy", "Złoty", "Zielonkawy", "Incognito", "Ospały", "Opieszały", "Zasapany"]
+var maleAdjectives = ["Zwinny", "Szybki", "Srebrny", "Różowy", "Złoty", "Zielonkawy", "Inkrustowany", "Ospały", "Opieszały", "Zasapany", "Zbzikowany", "Beztroski",
+"Wysportowany", "Zgrabny", "Dumny", "Wyjustowany", "Zdegustowany", "Zagubiona", "Szlachetny", "Nietuzinkowy", "Pomocny", "Spóźniony", "Zaspany", "Niewyspany",
+"Radioaktywny", "Szczery"
+]
 
-var femaleAdjectives = ["Miła", "Zgrabna", "Nieśmiała", "Złota", "Różowa", "Zielonkawa", "Szybka", "Sprytna"]
+var femaleAdjectives = ["Miła", "Zgrabna", "Nieśmiała", "Złota", "Różowa", "Zielonkawa", "Szybka", "Sprytna", "Beztroska", "Uważna", "Ostrożna", "Skupiona", "Duża",
+"Zwarta", "Pędząca", "Znudzona", "Niecierpliwa", "Skoncentrowana", "Zagubiona", "Nietuzinkowa", "Szlachetna", "Pomocna", "Zdolna", "Zaspana", "Spóźniona", "Niewyspana",
+"Tańcząca", "Pogodna", "Promieniująca", "Szczera"
+]
 
 var generate = function () {
-	if (Math.random() < 0.5){
+	if (Math.random() < 0.4){
 		var secondPart = maleAnimals[Math.floor(Math.random()*maleAnimals.length)];
 		var firstPart = maleAdjectives[Math.floor(Math.random()*maleAdjectives.length)];
 		console.log(firstPart+ " " +secondPart);
